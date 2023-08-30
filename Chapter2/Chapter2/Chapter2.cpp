8f6e3c1c3d458d897652e577c19552f4f4d21d3b
Eric Charnesky <eric.charnesky@careevolution.com>
2023-08-30 12:13:17 -0400

    arithmetic

--- Chapter2/Chapter2/Chapter2.cpp
+++ Chapter2/Chapter2/Chapter2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-using namespace std; int main() {
+using namespace std; 
+
+int main() {
 	cout << "Happy Wednesday!" << endl;	
 
 	cout << "It's nice to have internet again!" << endl;
@@ -21,6 +23,28 @@ using namespace std; int main() {
 		<< dollarsPerHour * 40 << " per week ( before taxes )" << endl
 		<< "That works out to $"
 		<< dollarsPerHour * 40 * 52 << " per year" << endl;
+
+	int firstNumber;
+	int secondNumber;
+
+	cout << "Enter your first interger: " << endl;
+	cin >> firstNumber;
+
+	cout << "Enter your second interger: " << endl;
+	cin >> secondNumber;
+
+	cout << "First number + second number : "
+		<< firstNumber + secondNumber << endl;
+
+	cout << "First number - second number : "
+		<< firstNumber - secondNumber << endl;
+
+	cout << "First number * second number : "
+		<< firstNumber * secondNumber << endl;
+
+	cout << "First number / second number : "
+		<< firstNumber / secondNumber  
+		<< " remainder: " << firstNumber % secondNumber << endl;
 
 
 }
