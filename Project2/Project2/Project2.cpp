b30739fd9b8d6f8c4dc00ee4ebdcaa61ab876599
Eric Charnesky <EricCharnesky@gmail.com>
2023-10-23 11:34:58 -0400

    project 2 started

--- Project2/Project2/Project2.cpp
+++ Project2/Project2/Project2.cpp
@@ -1,20 +1,82 @@
-// Project2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+string get_word() {
+    // TODO - FIXME
+    return "test";
+}
+
+bool has_the_word_been_guessed(string hidden_word, string letters_guessed) {
+    // for every letter in the hidden word
+    // check every letter in the letters guessed
+    // if it's not there, return false
+
+    // at the end, if you haven't returned false, return true
+    return false;
+}
+
+bool has_person_been_hanged(int number_of_incorrect_guesses) {
+    return false;
+}
+
+void display_hidden_word(string hidden_word, string letters_guessed) {
+    // for every letterin the hidden word, if it's been guessed, print the letter
+    // if it hasn't been guessed, print "_"
+    // for every letter in hidden word
+    // { 
+      // for every letter in letters gussed
+        // { 
+        //   if the letter matches
+          //   mark that it has been matched
+        // }
+       // if the letter matched, print letter
+       // otherwise print "_"
+     // }
+}
+
+char get_letter_guess(string letters_guessed) {
+    // get a guess, make sure it's 1 letter
+    // make sure it's not in the letters_guessed
+    return 'a';
+}
+
+bool is_letter_guessed_is_in_hidden_word(char letter_guessed, string hidden_word) {
+    // check every letter in hidden word to see if it matches letter_guessed
+    // for every letter in hidden_word
+    // {
+       // if you find it, return true
+    // }
+    // if we haven't returned true, we can return false
+    return false;
+}
+
+void print_gallows(int number_of_incorrect_guesses) {
+
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+   
+    string letters_guessed = "";
+    string hidden_word = get_word();
+    int number_of_incorrect_guesses = 0;
+
+    while (!has_the_word_been_guessed(hidden_word, letters_guessed) 
+        and !has_person_been_hanged(number_of_incorrect_guesses)) {
+        display_hidden_word(hidden_word, letters_guessed);
+        char guess = get_letter_guess(letters_guessed);
+        // either add the letter or update the letters_guessed in the fuction
+        letters_guessed += guess;
+
+        // check if the letter guessed is in the hidden word or not
+        if (!is_letter_guessed_is_in_hidden_word(guess, hidden_word)) {
+            number_of_incorrect_guesses += 1;
+        }
+
+        print_gallows(number_of_incorrect_guesses);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
