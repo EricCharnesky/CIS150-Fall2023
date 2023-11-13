3f4052e8c95fa8b051055e37fa20f13fc7657707
Eric Charnesky <EricCharnesky@gmail.com>
2023-11-13 16:31:04 -0500

    lab 8 started

---
+++ Lab8/Lab8/Lab8.cpp
@@ -0,0 +1,77 @@
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Item {
+private:
+    string name;
+    int quantity;
+    double unitPrice;
+public:
+    double getTotalPrice() {
+        return quantity * unitPrice;
+    }
+
+    string getName() {
+        return name;
+    }
+
+    int getQuantity() {
+        return quantity;
+    }
+
+    double getUnitPrice() {
+        return unitPrice;
+    }
+
+    void setName(string name) {
+        this->name = name;
+    }
+
+    void setQuantity(int quantity) {
+        if (quantity > 0) {
+            this->quantity = quantity;
+        }
+        else {
+            this->quantity = 1;
+        }
+    }
+
+    void setUnitPrice(double unitPrice) {
+        if (unitPrice > 0) {
+            this->unitPrice = unitPrice;
+        }
+        else {
+            this->unitPrice = 1;
+        }
+    }
+};
+
+int main()
+{
+    vector<Item> items;
+
+    Item item;
+    item.setName("whatever");
+    item.setQuantity(2);
+    item.setUnitPrice(1.5);
+
+    items.push_back(item);
+
+    double grandTotal = 0;
+    for (Item item : items) {
+        cout << item.getQuantity() << " " << item.getName() << " @ $"
+            << item.getUnitPrice() << " - " << item.getTotalPrice() << endl;
+        grandTotal += item.getTotalPrice();
+    }
+
+    //for (int index = 0; index < items.size(); index++) {
+    //    cout << items.at(index).getQuantity() << " " << items.at(index).getName() << " @ $"
+    //        << items.at(index).getUnitPrice() << " - " << items.at(index).getTotalPrice() << endl;
+    //}
+
+    cout << "Total: $" << grandTotal << endl;
+}
